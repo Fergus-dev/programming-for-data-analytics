030dde7c9d9e7257f91a13428621e8fe875576a1
Fergus McTiernan <fmtiernan@gmail.com>
2025-12-08 21:50:44 +0000

    final touches to weekly tasks handup

--- assignments/modified-assignment02-bankholidays.py
+++ assignments/modified-assignment02-bankholidays.py
@@ -1,22 +1,27 @@
 import requests
 import pandas as pd
+
+# below I use the requests library to get the json data from the gov.uk website
  
 url = "https://www.gov.uk/bank-holidays.json" 
 response = requests.get(url)
 response = response.json()
 
+# I create seperate dataframes of events in each jurisdiction by subsetting the response dictionary
+# I referred to page 188 of Wes McKinney's Python for Data Analysis while doing this
+
 northern_ireland_events = pd.DataFrame(response['northern-ireland']['events'], columns=['title', 'date'])
 print(northern_ireland_events)
 
 england_and_wales_events = pd.DataFrame(response['england-and-wales']['events'], columns=['title', 'date'])
-print(england_and_wales_events)
+#print(england_and_wales_events)
 
 scotland_events = pd.DataFrame(response['scotland']['events'], columns=['title', 'date'])
-print(scotland_events)
+#print(scotland_events)
 
-# cross reference data frames to find bank holidays unique to Northern Ireland
+#  I then cross reference data frames to find bank holidays unique to Northern Ireland
 # Copilot conversation for reference: https://copilot.microsoft.com/shares/wMh5d19vqpjtsBuyaonvQ
-# So the following code in English says, create a new data frame from whatever data that is in the events column of the norther ireland
+# So the following code in English says, create a new data frame from whatever data that is in the events column of the northern ireland
 # data frame and is not in the events column of the england and wales data frame and not in the events column of the scotland data frame
 # the ~ flips the boolean values. So although the .isin is being used, the ~ essentially makes it a "is not in" operation
 
